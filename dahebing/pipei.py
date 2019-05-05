d50b51be6b8ce22bb13df07c77f5b9826edde8b9
shuyuqing <40647826+shuyuqing@users.noreply.github.com>
2019-05-05 16:23:04 +0900

    Update pipei.py

--- dahebing/pipei.py
+++ dahebing/pipei.py
@@ -18,14 +18,28 @@ def read_out(file_path):
     for i in re.findall(r'\[.*\]', text):
 
         # print (re.findall(r'\[.*\]', text))
-        #
+        # 这里的话\就相当于/，在[的前面加上\就相当于我要匹配"["
+        # 这里的.匹配除了换行符之外的所有东西
+        # *是重复的意思，例如，ca*t 将匹配 'ct' (0个 'a' 字符)，'cat' (1个``'a')，` `'caaat' (3个 'a' 字符)，等等。
+        # findall返回的是一个列表
+        # split()会返回分割后的字符串列表。
+
 
 
         # print(i)
         # os.system("pause")
 
+
+        # "+的"意思是大于等于一个
+        # os.system("pause")
+        # (?P<value>\s+\d+\s+\d+),这里使用了捕获组来获取[]里面的值
+
+
+
         try:
             info = [int(i) + FRAMES_ADD - 1 for i in re.match(r'\[(?P<value>\s+\d+\s+\d+)\]',i).group('value').split()]
+            #match只匹配开头部分,函数group()可以返回被匹配到的字符串
+
 
         except:
             info = [int(i) + FRAMES_ADD - 1 for i in re.match(r'\[(?P<value>\d+\s+\d+)\]', i).group('value').split()]
@@ -34,6 +48,10 @@ def read_out(file_path):
         # os.system("pause")
 
         info_list_1 = [re.search(r'\s+\[(?P<value>.*)\]', i).group('value'), info]
+        #(?P<value>.*)是命名捕获组，value是这个捕获组的名称
+        # 这里使用捕获组来获取了[]里面的文字
+        #match是从开头开始进行匹配，而search是全部匹配
+
 
         # print(info_list_1)
         # os.system("pause")
@@ -46,8 +64,3 @@ def read_out(file_path):
     #最后的效果:[['', [0, 18]], ['お', [19, 24]], ['願い', [25, 49]], ['三', [50, 82]], ['。', [83, 86]]]
 
     #[  37   58]  0.562999  で+接続詞	[で]
-
-
-
-
-
