eb59f1b036e0ef4d47013bccfb2f1d20f3938d16
shu <782542967@qq.com>
2019-05-30 20:25:40 +0900

    14545

--- dahebing/Dahebing_shanchu.py
+++ dahebing/Dahebing_shanchu.py
@@ -1,18 +1,18 @@
 import pishan as pis
 
-path = r'C:\Users\a7825\Desktop\工作空间\杂物\对比\symbol'
+path = r'C:\Users\shu\Desktop\gongxiang\symbol'
 
 
 
 
 pis.pishan(path,guanjianzi='log',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
-pis.pishan(path,guanjianzi='log_biaoqian',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
-pis.pishan(path,guanjianzi='log_biaoqian_pingheng',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='mizhichuli',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='log_biaoqian',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
+# pis.pishan(path,guanjianzi='log_biaoqian_pingheng',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='mizhichuli',guanjianzi_1 = 'mulu')
 pis.pishan(path,guanjianzi='log_yuan',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='mizhichuli_biaoqian_pingheng',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='mizhichuli_biaoqian',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='mizhichuli_biaoqian_pingheng',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='mizhichuli_biaoqian',guanjianzi_1 = 'mulu')
 
-pis.pishan(path,guanjianzi='bulin',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='log_qian5',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='bulin',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='log_qian5',guanjianzi_1 = 'mulu')
 
