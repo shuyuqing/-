0faab6f79627e4128cdbfcd3f67d07511ef91535
惠普 <782542967@qq.com>
2019-05-30 20:33:41 +0900

    惠普

---
+++ dahebing/zhaocuo_1.py
@@ -0,0 +1,30 @@
+#作用于特征值文件，用于检查打标签的时候第一个空是不是全部被打上了1或者是0
+import csv,os
+import numpy as np
+
+def zhaocuo(path,guanjianzi):
+
+    for name in os.listdir(path):
+
+        path_xinde_log = os.path.join(path,name,guanjianzi)
+
+        for name_1 in os.listdir(path_xinde_log):
+
+            path_xinde_log_1 = os.path.join(path_xinde_log,name_1)
+
+            a = np.loadtxt(path_xinde_log_1, delimiter=',', skiprows=0).astype(np.float32)
+
+            Labeltrain = a[:, 0:1]
+
+            for i in Labeltrain:
+
+                if i!=1 or i!=0:
+
+                    print("这个文件的标签不是0或1")
+                    print(path_xinde_log_1)
+                    os.remove(path_xinde_log_1)
+
+                    break
+
+
+zhaocuo(path=r'C:\Users\a7825\Desktop\新建文件夹 (6)',guanjianzi=r'log_biaoqian')
