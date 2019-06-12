a995a2555a5c46d75cafe19a70b4aa4a9cd93a1d
惠普 <782542967@qq.com>
2019-06-12 16:33:56 +0900

    惠普

--- dahebing/Dahebing_shanchu.py
+++ dahebing/Dahebing_shanchu.py
@@ -1,18 +1,33 @@
 import pishan as pis
 
-path = r'C:\Users\a7825\Desktop\工作空间\杂物\对比\symbol'
+path = r'G:\研究生二年级\数据库\symbol_40_16_16_biaoqian_pingheng'
 
 
 
 
 # pis.pishan(path,guanjianzi='log',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
-pis.pishan(path,guanjianzi='log_biaoqian',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
-pis.pishan(path,guanjianzi='log_biaoqian_pingheng',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='mizhichuli',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='log_yuan',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='mizhichuli_biaoqian_pingheng',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='mizhichuli_biaoqian',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='log_biaoqian',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
+# pis.pishan(path,guanjianzi='log_biaoqian_pingheng',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='mizhichuli',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='log_yuan',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='mizhichuli_biaoqian_pingheng',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='mizhichuli_biaoqian',guanjianzi_1 = 'mulu')
 
-pis.pishan(path,guanjianzi='bulin',guanjianzi_1 = 'mulu')
-pis.pishan(path,guanjianzi='log_qian5',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='bulin',guanjianzi_1 = 'mulu')
+# pis.pishan(path,guanjianzi='log_qian5',guanjianzi_1 = 'mulu')
+
+
+
+
+
+#以下代码慎用
+
+# path_1 =r'G:\研究生二年级\数据库\symbol_40_16_16_biaoqian_pingheng'
+# pis.pishan(path_1,guanjianzi='mfcc',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
+# pis.pishan(path_1,guanjianzi='wav',guanjianzi_1 = 'mulu')#批量删除文件夹下的一些东西，注意，第二个关键字根据要删除的是文件（wenjian）还是目录(mulu)来决定
+# pis.pishan(path_1,guanjianzi='keka',guanjianzi_1 = 'mulu')
+# pis.pishan(path_1,guanjianzi='wav_all',guanjianzi_1 = 'mulu')
+# pis.pishan(path_1,guanjianzi='log_yuan',guanjianzi_1 = 'mulu')
+# pis.pishan(path_1,guanjianzi='mizhichuli_biaoqian_pingheng',guanjianzi_1 = 'mulu')
+# pis.pishan(path_1,guanjianzi='mizhichuli_biaoqian',guanjianzi_1 = 'mulu')
 
