e97c95f90ae9af002a756eaab3be3e289a3f8420
shuyuqing <40647826+shuyuqing@users.noreply.github.com>
2019-06-14 12:43:22 +0900

    Update Dahebing_a_fbank.py

--- dahebing/Dahebing_a_fbank.py
+++ dahebing/Dahebing_a_fbank.py
@@ -103,11 +103,11 @@ if zhengguihua_2 == True:
 
 
 
-# pa.dabiaoqian(path,guanjianzi_1 = s1,guanjianzi_2 = s1+'_'+'biaoqian')#打标签
+# pas.dabiaoqian(path,guanjianzi_1 = s1,guanjianzi_2 = s1+'_'+'biaoqian')#打标签
 zx.zuixiao(path,guanjianzi=s1+'_'+'biaoqian',xiaxian=10)
 # bl.kongwenjian(path,guanjianzi=s1+'_'+'biaoqian')#把大小为0的文件都删除了
-# bl.pingheng(path,guanjianzi= s1+'_'+'biaoqian')#把标签全部是0的文件都移动到桌面去
-# bl.pingheng_1(path,guanjianzi=s1+'_'+'biaoqian')#把标签全部是1的文件都移动到桌面上去
+# bl.pingheng_2(path,guanjianzi= s1+'_'+'biaoqian')#把标签全部是0的文件都移动到桌面去
+# bl.pingheng_3(path,guanjianzi=s1+'_'+'biaoqian')#把标签全部是1的文件都移动到桌面上去
 s1 = s1+'_'+'biaoqian'
 dataname_1 = dataname_1 + '_' + 'biaoqian'
 dataname_2 = dataname_2 + '_' + 'biaoqian'
@@ -122,7 +122,7 @@ dataname_2 = dataname_2 + '_' + 'biaoqian'
 #     for wenjian_1 in os.listdir(path_1):
 #         path_2 = os.path.join(path_1, wenjian_1)
 #         qie.qiexiao(path_2,wenjian_1,path_new)
-# bl.pingheng(path, guanjianzi=s1+'_'+'pingheng')  # 把标签全部是0的文件都移动到桌面去,因为切割之后会留下很多标签全是0的文件
+# bl.pingheng_2(path, guanjianzi=s1+'_'+'pingheng')  # 把标签全部是0的文件都移动到桌面去,因为切割之后会留下很多标签全是0的文件
 zx.zuixiao(path,guanjianzi=s1+'_'+'pingheng',xiaxian=10)
 s1 = s1+'_'+'pingheng'
 dataname_1 = dataname_1 + '_' + 'pingheng'
