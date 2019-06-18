b1f7f087be8fb4427ec22d3e3b959e6c0684f201
shuyuqing <40647826+shuyuqing@users.noreply.github.com>
2019-06-19 07:46:58 +0900

    Update jiachuang.py

--- dahebing/jiachuang.py
+++ dahebing/jiachuang.py
@@ -8,9 +8,9 @@ def jiachuangzi(a):
     window = np.hamming(len(a))
     n=0
     for i in window:
-        print(i)
+        
         a[n] = a[n]*i
-        print(a[n])
+        
         n = n+1
 
     return a
